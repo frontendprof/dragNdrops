72d9b1a514ff2a511ffd11c29192749a2f318dab
AbdulMalik <abdumaliksharipov@gmail.com>
2020-08-07 23:54:31 +0500

    Complete

--- dragnDropAPI-2/app.js
+++ dragnDropAPI-2/app.js
@@ -15,16 +15,60 @@ draggablElements.forEach(e=>{
 
 
 droppablElements.forEach(e=>{
+    e.addEventListener("dragenter",dragEnter);
     e.addEventListener("dragover",dragOver);
-    // e.addEventListener("drop",drop);
+    e.addEventListener("dragleave",dragLeave);
+    e.addEventListener("drop",drop);
 })
 
 
+
+// Draggable events
+
 function dragStart(e){
-    console.log(e.dataTransfer.setData("text", e.target.color));
-    e.dataTransfer.setData("text",e.target.color);
+    console.log(e.dataTransfer.setData("text", e.target.id));
+    e.dataTransfer.setData("text",e.target.id);
+}
+
+
+
+// Droppable events
+
+function dragEnter(e){
+    if(!e.target.classList.contains("dropped")){
+
+        e.target.classList.add("droppable-hover");
+    }
 }
 
 function dragOver(e){
+    if(!e.target.classList.contains("dropped")){
+
+        e.preventDefault();
+    }
+    
+}
+
+
+function dragLeave(e){
+    e.target.classList.remove("droppable-hover");
+}
+
+function drop(e){
     e.preventDefault();
+    e.target.classList.remove("droppable-hover")
+    const draggablElementData=e.dataTransfer.getData("text");
+    const droppablElementData=e.target.getAttribute("data-draggable-id");
+
+    if(draggablElementData===droppablElementData){
+        e.target.classList.add("dropped");
+        const draggablElement=document.getElementById(draggablElementData);
+        e.target.style.backgroundColor = draggablElement.style.color;
+        console.log(draggablElement.style.color)
+        draggablElement.classList.add("dragged");
+        draggablElement.setAttribute("draggable","false");
+        e.target.insertAdjacentHTML("afterbegin",`<i class="fas fa-${draggablElementData}"></i>`);        
+
+    }
+
 }
