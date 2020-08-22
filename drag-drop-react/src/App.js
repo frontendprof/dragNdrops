bf31c676d938a564e6c471e693c54ac336ea3cf9
AbdulMalik <abdumaliksharipov@gmail.com>
2020-08-22 21:57:08 +0500

    react drag and drop setup

---
+++ drag-drop-react/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <main className="flexbox">
+
+        <h1>Assalaam aleykom</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
