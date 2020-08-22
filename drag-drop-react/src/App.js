011f9255a2199f27c920ba56d16f642ea7e68af0
AbdulMalik <abdumaliksharipov@gmail.com>
2020-08-22 23:10:45 +0500

    Styling drag and drop

--- drag-drop-react/src/App.js
+++ drag-drop-react/src/App.js
@@ -8,9 +8,6 @@ function App() {
     <div className="App">
       <main className="flexbox">
 
-        <h1>Assalaam aleykom</h1>
-
-
         <Board id="board-1" className="board">
           <Card id="card-1" className="card" draggable='true'>
             <p>Card One</p>
@@ -20,12 +17,6 @@ function App() {
         <Board id="board-2" className="board">
           <Card id="card-2" className="card" draggable='true'>
             <p>Card Two</p>
-          </Card>
-        </Board>
-
-        <Board id="board-3" className="board">
-          <Card id="card-3" className="card" draggable='true'>
-            <p>Card Three</p>
           </Card>
         </Board>
 
