1ecb2ff7bf6ac9b95b42abfe552e778ecba358fa
AbdulMalik <abdumaliksharipov@gmail.com>
2020-08-24 23:49:11 +0500

    Creating card component

---
+++ vue-drag-drop/src/components/Card.vue
@@ -0,0 +1,29 @@
+<template>
+    <div
+        :id="id"
+        class="card"
+        :draggable="draggable"
+        @dragstart="dragStart"
+        @dragover.stop
+        @drop.prevent='drop'
+    >
+        <slot/>
+    </div>
+</template>
+
+
+<script>
+export default {
+    props:["id","draggable"],
+    methods:{
+        drop:e=>{
+            const card_id=e.dataTransfer.getData("card_id");
+
+            const card=document.getElementById(card_id);
+
+            card.style.display="block";
+            e.target.appendChild(card);
+        }
+    }
+}
+</script>
